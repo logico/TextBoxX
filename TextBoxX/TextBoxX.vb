0933d124fbc84a094ec81a0ffee04805375604ed
logico <aamaral@logico.com.ar>
2015-03-10 14:28:09 -0300

    Creada propiedad MoneyField que trata al campo como un campo con formato de dinero. Agregada funcion GetMoneyValue que devuelve cadena sin formato

--- TextBoxX/TextBoxX.vb
+++ TextBoxX/TextBoxX.vb
@@ -1,4 +1,7 @@
-﻿Option Explicit On
+﻿' Money field code by user shavinder on CodeProject
+' http://www.codeproject.com/Tips/311959/Format-a-textbox-for-currency-input-VB-NET
+
+Option Explicit On
 
 Imports System
 Imports System.Drawing
@@ -9,6 +12,9 @@ Imports System.Text.RegularExpressions
 
 Public Class TextBoxX
     Inherits System.Windows.Forms.TextBox
+
+    Dim strCurrency As String = ""
+    Dim acceptableKey As Boolean = False
 
     Private Const ECM_FIRST As Long = &H1500
     Private Const EM_SETCUEBANNER As Long = (ECM_FIRST + 1)
@@ -29,11 +35,31 @@ Public Class TextBoxX
     '--Only Numbers
     Private _onlyNumbers As Boolean = False
 
+    Private _MoneyField As Boolean = False
+
+    ' -- Currency field
+    Public Property MoneyField() As Boolean
+        Get
+            Return _MoneyField
+        End Get
+        Set(value As Boolean)
+            If value = True Then
+                Me.TextAlign = HorizontalAlignment.Right
+                OnlyNumbers = False
+            End If
+            _MoneyField = value
+        End Set
+    End Property
+
+
     Public Property OnlyNumbers() As Boolean
         Get
             Return _onlyNumbers
         End Get
         Set(value As Boolean)
+            If value = True Then
+                MoneyField = False
+            End If
             _onlyNumbers = value
         End Set
     End Property
@@ -74,16 +100,27 @@ Public Class TextBoxX
 
     'Check if the text match with the pattern of the regular expression
     Public Function IsValid() As Boolean
-        If (_IsRequired = True) And (Me.Text.Length = 0) Then
-            Return False
-        End If
-        If (Me._regexString.Length <> 0) And (Me.Text.Length <> 0) Then     'If the regex string and the content of the text box are not empty
+
+        ' Devuelve true porque se analiza un campo que no es necesario
+        If _IsRequired = False Then Return True
+
+        ' Si es requerido Y no tiene texto = fallo
+        If (_IsRequired = True) And (Me.Text.Length = 0) Then Return False
+
+        ' If required and acept any string and the text is not empty = aprobed
+        If (_IsRequired) And (Me._regexString = "*") And (Me.Text.Length <> 0) Then Return True
+
+
+        If (Me.IsRequired) And (Me._regexString.Length <> 0) And (Me.Text.Length <> 0) Then     'If the regex string and the content of the text box are not empty
 
             Dim regexObj As Regex = New Regex(_regexString)
             Dim match As Match = regexObj.Match(Me.Text)
 
+            Debug.Print(Name)
             Return match.Success                                        'Return if match
+
         Else
+            Debug.Print(Name)
             Return False
         End If
     End Function
@@ -98,6 +135,19 @@ Public Class TextBoxX
         End Set
     End Property
 
+    ''' <summary>
+    ''' Devuelve la cadena para ser convertida en decimal cuando se ingresa el campo de moneda
+    ''' </summary>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Public Function GetMoneyValue() As String
+        If _MoneyField And Not Text.Length = 0 Then
+            Return Text.Replace("$ ", vbNullString)
+        Else
+            Return Text
+        End If
+    End Function
+
     Protected Overloads Overrides Sub WndProc(ByRef m As Message)
         Select Case m.Msg
             Case WM_PAINT
@@ -110,7 +160,7 @@ Public Class TextBoxX
         End Select
     End Sub
 
- 
+
 
     Protected Overloads Sub OnPaint()
         Dim rcItem As Rectangle = New Rectangle(0, 0, Me.Bounds.Width, Me.Bounds.Height)
@@ -137,9 +187,78 @@ Public Class TextBoxX
     End Sub
 
 
+
+    Private Sub TextBoxX_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+
+        If (e.KeyCode >= Keys.D0 And e.KeyCode <= Keys.D9) OrElse (e.KeyCode >= Keys.NumPad0 And e.KeyCode <= Keys.NumPad9) OrElse e.KeyCode = Keys.Back OrElse e.KeyCode = Keys.Decimal OrElse e.KeyValue = 46 OrElse e.KeyCode = Keys.Oemcomma Then
+
+            acceptableKey = True
+            If e.KeyCode = 46 And _MoneyField Then
+                strCurrency = ""
+                Me.Clear()
+            End If
+
+            If e.KeyCode = Keys.Decimal And _MoneyField Then
+                e.SuppressKeyPress = True
+                SendKeys.Send(",")
+            End If
+
+        Else
+            acceptableKey = False
+        End If
+    End Sub
+
     Private Sub TextBoxX_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Me.KeyPress
-        If (_onlyNumbers = True) And ((Asc(e.KeyChar)) <> 8) And (IsNumeric(e.KeyChar) = False) Then
+
+        If _onlyNumbers Then
+            If ((Asc(e.KeyChar)) <> 8) And (IsNumeric(e.KeyChar) = False) Then
+                e.Handled = True
+            End If
+        End If
+
+        If _MoneyField Then
+            If acceptableKey = False Then
+                ' Stop the character from being entered into the control since it is non-numerical.
+                e.Handled = True
+                Return
+            Else
+
+                If e.KeyChar = Convert.ToChar(Keys.Back) Then
+                    If strCurrency.Length > 0 Then
+                        strCurrency = strCurrency.Substring(0, strCurrency.Length - 1)
+                    End If
+                Else
+                    strCurrency = strCurrency & e.KeyChar
+                End If
+
+                If strCurrency.Length = 0 Then
+                    Me.Text = ""
+                ElseIf strCurrency.Length = 1 Then
+                    Me.Text = "0,0" & strCurrency
+                ElseIf strCurrency.Length = 2 Then
+                    Me.Text = "0," & strCurrency
+                ElseIf strCurrency.Length > 2 Then
+                    Me.Text = strCurrency.Substring(0, strCurrency.Length - 2) & "," & strCurrency.Substring(strCurrency.Length - 2)
+                End If
+                Me.Select(Me.Text.Length, 0)
+            End If
             e.Handled = True
+        End If
+
+    End Sub
+
+    Private Sub TextBoxX_GotFocus(sender As Object, e As EventArgs) Handles Me.GotFocus
+        If _MoneyField Then
+            If Text.StartsWith("$ ") Then
+                Text = Text.Replace("$ ", "")
+                Me.SelectAll()
+            End If
+        End If
+    End Sub
+
+    Private Sub TextBoxX_LostFocus(sender As Object, e As EventArgs) Handles Me.LostFocus
+        If _MoneyField And Not Text.Length = 0 Then
+            Text = "$ " & Text
         End If
     End Sub
 End Class
